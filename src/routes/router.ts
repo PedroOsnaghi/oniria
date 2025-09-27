1f1c0711fbfb599606476fc34f907fde32c36fd7
maertin <mutuverria00gmail.com>
2025-09-27 19:09:32 -0300

    Add root endpoint

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -4,9 +4,10 @@ import { dreamNodeRouter } from "./modules/dream-node.routes";
 export class AppRoutes {
     static get routes(): Router {
         const router = Router();
-        
+
+        router.use("/", (req, res) => res.send("Oniria API"));
         router.use("/api/dream-node", dreamNodeRouter);
-        
+
         return router;
     }
 }
