e45aeed39edb92dcb23f4d45484f0d68fe13c51f
KevinLlombart <143675096+KevinLlombart@users.noreply.github.com>
2025-10-11 02:19:24 -0300

    feature: content moderation implementation

--- src/infrastructure/routes/modules/dream-node.routes.ts
+++ src/infrastructure/routes/modules/dream-node.routes.ts
@@ -5,6 +5,7 @@ import { InterpretationDreamService } from "../../../application/services/interp
 import { DreamNodeService } from "../../../application/services/dream-node.service";
 import { DreamNodeRepositorySupabase } from "../../repositories/dream-node.repository.supabase";
 import { validateBody, validateParams, validateQuery } from "../../middlewares/validate-class.middleware";
+import contentModerationMiddleware from '../../middlewares/content-moderation.middleware';
 import { InterpreteDreamRequestDto, ReinterpreteDreamRequestDto, SaveDreamNodeRequestDto } from "../../dtos/dream-node";
 import { GetUserNodesParamsDto, GetUserNodesQueryDto } from "../../dtos/dream-node/get-user-nodes.dto";
 
@@ -17,7 +18,7 @@ const dreamNodeService = new DreamNodeService(dreamNodeRepository);
 const dreamNodeController = new DreamNodeController(interpretationDreamService, dreamNodeService);
 
 // Endpoints de interpretación
-dreamNodeRouter.post("/interpret", validateBody(InterpreteDreamRequestDto), (req, res) => dreamNodeController.interpret(req, res));
-dreamNodeRouter.post("/reinterpret", validateBody(ReinterpreteDreamRequestDto), (req, res) => dreamNodeController.reinterpret(req, res));
+dreamNodeRouter.post("/interpret", validateBody(InterpreteDreamRequestDto), contentModerationMiddleware, (req, res) => dreamNodeController.interpret(req, res));
+dreamNodeRouter.post("/reinterpret", validateBody(ReinterpreteDreamRequestDto), contentModerationMiddleware, (req, res) => dreamNodeController.reinterpret(req, res));
 dreamNodeRouter.post("/save", validateBody(SaveDreamNodeRequestDto), (req, res) => dreamNodeController.save(req, res));
 dreamNodeRouter.get("/user/:userId", validateParams(GetUserNodesParamsDto), validateQuery(GetUserNodesQueryDto), (req, res) => dreamNodeController.getUserNodes(req, res));
